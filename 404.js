c7cf6f15b431b44e8eec7b1c0e7bf741ff439554
israelDeLazaroMartinez <rafhasoluciones27@gmail.com>
2026-05-27 16:48:56 -0600

    se inicializo vite, y se elimino los archivos estaticos y paso los archivos estaticos a vite

--- 404.js
+++
@@ -1,9 +0,0 @@
-// Botón Volver - history.back() forzoso
-const backButton = document.getElementById('backButton');
-
-if (backButton) {
-    backButton.addEventListener('click', (e) => {
-        e.preventDefault(); // Evita que el href="#" haga scroll al top
-        window.history.back();
-    });
-}
